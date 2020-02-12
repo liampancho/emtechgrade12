005a553368125b382750dc7000b6984ee68c49fd
Jan William Pancho <52308415+liampancho@users.noreply.github.com>
2020-02-12 21:43:01 +0800

    Update MagnitudeDirection.cpp

--- MagnitudeDirection.cpp
+++ MagnitudeDirection.cpp
@@ -4,19 +4,24 @@
 using namespace std;
 int main()
 {  
-   float m1,m2,d1,d2,w,x,y,z,s,p,r,d;
+   float mag1,mag2,mag3,dir1,dir2,dir3,Ax,Ay,Bx,By,Cx,Cy,Rx,Ry,Resultant,Direction;
    float pi=3.1416/180.0;
-   float rpi=180/3.1416;
+   float rpi=180.0/3.1416;
    cout<<fixed<<setprecision(2);
    cout<<"Computing for the Resultant and Direction given the Magnitude and Direction"<<endl;
-   cout<<endl<<"Enter the Magnitude:  ";
-   cin>>m1>>m2;
+   cout<<endl<<"Enter the Magnitude:  "<<endl;
+   cin>>mag1>>mag2>>mag3;
    cout<<endl<<"Enter the Direction:  ";
-   cin>>d1>>d2;
-   w=m1*cos(m2*pi); x=m1*sin(m2*pi); y=d1*cos(d2*pi);
-   z=d1*sin(d2*pi); s=w+y; p=x+z;
-   r=(sqrt(pow(s,2)+pow(p,2)));
-   d=atan(p/s)*(rpi);
-   cout<<endl<<"Resultant=  "<<r<<endl<<"Direction=  "<<d<<endl;
+   cin>>dir1>>dir2>>dir3;
+   Ax=mag1*cos(dir1*pi); 
+   Ay=mag1*sin(dir1*pi);
+   Bx=mag2*cos(dir2*pi);
+   By=mag2*sin(dir2*pi);
+   Cx=mag3*cos(dir3*pi);
+   Cy=mag3*sin(dir3*pi);
+   Rx=Ax+Bx+Cx;
+   Ry=Ay+By+Cy;
+   Resultant=(sqrt(pow(Rx,2)+pow(Ry,2)));
+   Direction=(atan(Ry/Rx))*(rpi);
+   cout<<endl<<"Resultant=  "<<Resultant<<endl<<"Direction=  "<<Direction<<endl;
 }
-
